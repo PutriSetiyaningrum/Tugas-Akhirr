93be70d376a5cb540c83ffc2bd9a1c20ca84aa02
putri <putrisetiyaningrum99@gmail.com>
2023-04-11 16:09:14 +0700

    PUSH

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/coba", function() {
+    echo "ada";
+});
+
 Route::get('/', function () {return view('/user/landingpage/home');});
 Route::get('/s&k', function () {return view('/user/landingpage/s&k');});
 Route::get('/kontak', function () {return view('/user/landingpage/kontak');});
