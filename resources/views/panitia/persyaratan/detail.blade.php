038b1e2125255497e9dd64855b91cf93d3d63f10
putri <putrisetiyaningrum99@gmail.com>
2023-06-04 12:22:14 +0700

    Update DataTable

--- resources/views/panitia/persyaratan/detail.blade.php
+++ resources/views/panitia/persyaratan/detail.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.main')
+
+@section("css")
+<link rel="stylesheet" href="{{ url('/AdminLTE') }}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ url('/AdminLTE') }}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ url('/AdminLTE') }}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+@endsection
+
 @section('content')
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -23,7 +30,7 @@
             </div>
             @endcan
             <div class="card-body">
-                <table class="table table-bordered">
+                <table id="example1" class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th style="width: 10px">No</th>
@@ -98,3 +105,19 @@
 </div>
 @endsection
 
+@section("js")
+
+<script src="{{ url('/adminLTE') }}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ url('/adminLTE') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ url('/adminLTE') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ url('/adminLTE') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ url('/adminLTE') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{ url('/adminLTE') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script>
+    $(function () {
+        $("#example1").DataTable({
+            "responsive": true, "lengthChange": false, "autoWidth": false,
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
+</script>
+@endsection
