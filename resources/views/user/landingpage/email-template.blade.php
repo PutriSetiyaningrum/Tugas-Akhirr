b230d367de46166dac1f8196c195606353aff44e
putri <putrisetiyaningrum99@gmail.com>
2023-07-18 11:53:15 +0700

    benerin tulisan di pemberitahuan email

--- resources/views/user/landingpage/email-template.blade.php
+++ resources/views/user/landingpage/email-template.blade.php
@@ -154,7 +154,7 @@
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Confirm Your Email Address</h1>
+              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Konfirmasi Alamat Email Mu</h1>
             </td>
           </tr>
         </table>
@@ -196,7 +196,7 @@
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
                           <a href="{{$actionLink}}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro',
-                          Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Do Something Sweet</a>
+                          Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Klik di Sini Untuk Verifikasi Email</a>
                         </td>
                       </tr>
                     </table>
