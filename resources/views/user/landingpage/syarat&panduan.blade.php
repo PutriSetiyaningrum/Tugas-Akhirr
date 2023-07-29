5e15185787c9ca8e53c08aaa2515b1b4b2bef88a
putri <putrisetiyaningrum99@gmail.com>
2023-07-29 21:17:50 +0700

    Fix terakhir final

--- resources/views/user/landingpage/syarat&panduan.blade.php
+++ resources/views/user/landingpage/syarat&panduan.blade.php
@@ -23,15 +23,15 @@
                         <div class="title-contact">Syarat </div>
                         <p>1. Pelatih menyiapkan scan surat rekomendasi dari kepala sekolah untuk nama pemain dan official berupa soft file PDf</p><br>
                         <p>2. Pada saat pengisian persyaratan wajib mengirim logo sekolah </p><br>
-                        <p>3. Untuk peserta SMA/SMK/MA Divisi 1 Putra berumlah 15 pemain dan untuk SD/MI, SMP/MTS, CLUB dan SMA/SMK/MA Divisi 2 Putri berjumlah 12 pemain</p><br>
+                        <p>3. Untuk peserta SMA/SMK/MA Divisi 1 Putra berjumlah 15 pemain dan untuk SD/MI, SMP/MTS, CLUB dan SMA/SMK/MA Divisi 2 Putri berjumlah 12 pemain</p><br>
                         <p>4. Semua persyaratan team berupa foto copy dan dikirim berupa soft file PDF (foto, logo sekolah, ijazah, akte, dan form Pendaftaran) pada form persyaratan yang didaftarkan pelatih</p><br>
-                        <p>5. Informasi Mengenai Event atau biaya pendaftaran bisa dilihat pada menu Event</p><br>
+                        <p>5. Informasi Mengenai Event atau biaya pendaftaran bisa dilihat pada menu Informasi, kemudian pilih Tentang Event</p><br>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-5">
                     <div class="box">
                         <div class="title-contact">Panduan</div>
-                        <p>1. Pelatih wajib mempunyai akun yang sudah didaftarkan panitia untuk mengisi data persyaratan </p><br>
+                        <p>1. Pelatih wajib mempunyai akun yang sudah didaftarkan pengurus untuk mengisi data persyaratan </p><br>
                         <p>2. Buka Website PERBASI Indramayu</p><br>
                         <p>3. Log in untuk bisa mengakses Persyaratan Team</p><br>
                         <p>4. Pilih menu Event, lalu klik Isi Persyaratan</p><br>
